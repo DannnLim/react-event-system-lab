2b2bc67812267027115269b38d1f856c638fd700
DannnLim <Dannylim-@hotmail.com>
2022-08-15 19:55:25 +1000

    done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,23 @@
 // Code EyesOnMe Component Here
+import { render } from "enzyme";
+import React,{ Component} from "react";
+
+export default class EyesOnMe extends React.Component{
+    handleFocus = () =>{
+        console.log("Good!")
+    };
+    
+    handleBlur = () =>{
+        console.log("Hey! Eyes on me!");
+    };
+    
+render(){
+    return (
+        
+        <button onFocus={this.handleFocus} onBlur={this.handleBlur}>button</button>
+    )
+}
+}
+
+
+
